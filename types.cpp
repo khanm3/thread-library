61ce7c80720b9a6e84684e610ec31dfcf6505ae2
Agatha Ta-Goetz <tata@umich.edu>
2021-10-05 15:23:55 -0400

    add finishedList

--- types.cpp
+++ types.cpp
@@ -1,14 +1,19 @@
-#include <ucontext.h>
 #include "types.h"
 
 std::queue<Tcb> readyQueue;
+std::vector<Tcb> finishedList;
 
 void os_wrapper(thread_startfunc_t body, void *arg) {
+    assert_interrupts_disabled();
     // do os stuff
+
+    // If there are any finished threads to clean up, clean them up
+    while(!finishedList.empty()) {
+        finishedList.pop_back();
+    }
 
     // enable interrupts - switch invariant
     cpu::interrupt_enable();
-    // If there are any finished threads to clean up, clean them up
     // TODO: MULTIPROCESSOR - switch invariant - acquire guard
 
     // run thread to finish
@@ -17,12 +22,15 @@ void os_wrapper(thread_startfunc_t body, void *arg) {
     // disable interrupts - switch invariant
     cpu::interrupt_disable();
 
+    // TODO: Move current tcb from runningList to finishedList
+
     // if another thread on ready queue, switch to it
     if (!readyQueue.empty()) {
         Tcb threadToRun;
         threadToRun = std::move(readyQueue.front());
         readyQueue.pop();
 
+        // concern: threadToRun might go out of context and then we would leak its stack
         setcontext(threadToRun.ctx.get());
     }
 }
