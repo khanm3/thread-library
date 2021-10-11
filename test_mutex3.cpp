4cb013c1759aab20070809066f588d08fcf01052
Agatha Ta-Goetz <tata@umich.edu>
2021-10-10 22:22:57 -0400

    added mutext test for error handling

---
+++ test_mutex3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "thread.h"
+
+void goodbye(void*);
+void hello(void *);
+
+mutex m;
+
+void goodbye(void* a)
+{
+    m.lock();
+    std::cout << "locked once" << (intptr_t)a << "\n";
+    m.lock();
+    std::cout << "locked twice" << (intptr_t)a << "\n";
+    m.unlock();
+    std::cout << "unlocked once" << (intptr_t)a << "\n";
+    m.unlock();
+    std::cout << "unlocked twice" << (intptr_t)a << "\n"; // error should be thrown before this prints
+}
+
+void hello(void *a)
+{
+    std::cout << "Hello, world!" << std::endl;
+
+    intptr_t one = 1;
+    thread t1((thread_startfunc_t) goodbye, (void *) one);
+}
+
+int main()
+{
+    cpu::boot(1, (thread_startfunc_t) hello, (void *) 100, false, false, 0);
+    std::cout << "finishing boot" << std::endl;
+}
