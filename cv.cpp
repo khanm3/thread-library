d5ba7f188cfb5cd28962a72c8385ddf00cdd24bc
Gabriel Bornstein <gborn@umich.edu>
2021-10-13 16:33:45 -0400

    cv first implementation

--- cv.cpp
+++ cv.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
 #include "cv.h"
 #include "types.h"
+#include "mutexImpl.h"
 
 class cv::impl {
     public:
@@ -18,20 +19,41 @@ cv::~cv() {
 
 void cv::wait(mutex &m) {
     // Mutex checks for proper owner
-    m.unlock();
+    cpu::interrupt_disable();
+
+    m.impl_ptr->unlockHelper();
     
     assert(runningList.find(cpu::self()) != runningList.end());
     TcbPtr &currThread = runningList[cpu::self()];
     impl_ptr->waitQueue.push(std::move(currThread));
 
     switch_to_next_or_suspend(&impl_ptr->waitQueue.back()->ctx);
-    m.lock();
+
+    m.impl_ptr->lockHelper();
+
+    cpu::interrupt_enable();
 }
 
 void cv::signal() {
+    cpu::interrupt_disable();
 
+    if (!impl_ptr->waitQueue.empty()) {
+        TcbPtr &threadToRun = impl_ptr->waitQueue.front();
+        readyQueue.push(std::move(threadToRun));
+        impl_ptr->waitQueue.pop();
+    }
+
+    cpu::interrupt_enable();
 }
 
 void cv::broadcast() {
+    cpu::interrupt_disable();
 
+    while (!impl_ptr->waitQueue.empty()) {
+        TcbPtr &threadToRun = impl_ptr->waitQueue.front();
+        readyQueue.push(std::move(threadToRun));
+        impl_ptr->waitQueue.pop();
+    }
+    
+    cpu::interrupt_enable();
 }
