4f5683265dd0fc56da65e091fe0f3d98932ff89d
mohkha <mohkha@umich.edu>
2021-10-19 06:55:54 -0400

    Created a test for mutex style that exposes a bug in mutex::unlock()
    Currently we keep track of a mutex's owner using the raw Tcb pointer of the thread
    that owns it. If a thread exits without unlocking a mutex, the following situation
    can occur:
    
    The thread that exited holding the mutex is freed. Then a different thread is created
    and its Tcb is allocated at the same location as the thread which exited. This thread
    can then call unlock on the mutex succesfully since its Tcb pointer is the same as
    the Tcb pointer the mutex is keeping track of.
    
    The test case added recreates the situation explained above. We will need to fix this
    bug in the future. Also this bug is likely related to test case 19 on the autograder,
    refer to @849 and @755 on Piazza.

---
+++ tests/test_style_mutex1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdexcept>
+#include "thread.h"
+
+mutex m;
+
+void func0(void *a) {
+    intptr_t i = (intptr_t) a;
+    std::cout << i << " pre-lock" << std::endl;
+    m.lock();
+    std::cout << i << " exit while holding lock" << std::endl;
+}
+
+void func1(void *a) {
+    intptr_t i = (intptr_t) a;
+    std::cout << i << " attempting to unlock" << std::endl;
+    m.unlock();
+    std::cout << i << " exit after unlock" << std::endl;
+}
+
+void start_func(void *a) {
+    if (true) {
+        thread t0((thread_startfunc_t) func0, (void *) 0);
+        thread::yield();
+    }
+
+    try {
+        thread t1((thread_startfunc_t) func1, (void *) 1);
+    }
+    catch (const std::runtime_error &re) {
+        std::cout << "runtime error: 1 tried to free a lock that 0 holds" << std::endl;
+        assert_interrupts_disabled();
+        return;
+    }
+}
+
+int main() {
+    cpu::boot(1, (thread_startfunc_t) start_func, (void *) 0, false, false, 0);
+}
